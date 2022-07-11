abe8d8fcb5281e38cda9c4ccf44079a96b25f513
jinhua.xu <1569253760@qq.com>
2022-07-11 14:27:34 +0800

    module操作

---
+++ com/codeh/module/mod1.py
@@ -0,0 +1,17 @@
+import math
+from math import sqrt
+from math import *
+import time as t
+from time import sleep as s
+
+# 控制允许导入的模块列表
+__all__ = []
+
+print(math.fabs(-1))
+print(sqrt(2))
+print(ceil(3))
+
+t.sleep(2)
+print('hello')
+s(1)
+print('hello1')
